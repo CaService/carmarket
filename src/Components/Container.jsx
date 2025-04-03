26f93d2843f35ae52ee36dd9640a66b995949bef
CaService <ca.service2025@proton.me>
2025-04-04 00:23:08 +0200

    working on navbar resoponsiveness

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-  return <div className="max-w-[1920px] px-40 w-full">{children}</div>;
+  return <div className="max-w-[1920px] px-4 md:px-40 w-full">{children}</div>;
 };
 
 export default Container;
