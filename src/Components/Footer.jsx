b1cbcd17e6412219ab200fb332795678e8e3df38
CaService <ca.service2025@proton.me>
2025-04-04 03:43:17 +0200

    working on footer responsiveness

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,21 +6,21 @@ const Footer = () => {
     <footer className="bg-[#072534] text-white text-sm pt-10 pb-5">
       <Container>
         <div className="max-w-7xl mx-auto px-4">
-          <div className="flex flex-col md:flex-row justify-between items-start mb-8 space-y-8 md:space-y-0">
-            <div className="flex items-center w-full md:w-auto justify-center md:justify-start">
+          <div className="flex flex-col md:flex-row justify-between items-start mb-8">
+            <div className="flex items-center justify-center w-full md:w-auto mb-8 md:mb-0">
               <img
                 src="/images/logo-desktop-ayvens-white.svg"
                 alt="Ayvens Logo"
-                className="h-20 md:h-24"
+                className="h-24"
               />
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-2 gap-8 w-full">
+            <div className="grid grid-cols-2 md:grid-cols-2 gap-8 w-full md:w-auto">
               <div className="text-center md:text-left">
                 <h2 className="text-white text-xl font-medium mb-4">
                   Chi Siamo
                 </h2>
-                <ul className="space-y-3 font-medium font-['Source_Sans_Pro'] text-base">
+                <ul className="space-y-3 md:space-y-1 font-medium font-['Source_Sans_Pro'] text-base">
                   <li>
                     <a href="#" className="font-medium">
                       La società
@@ -38,7 +38,7 @@ const Footer = () => {
                 <h2 className="text-white font-medium mb-4 text-xl">
                   Help Center
                 </h2>
-                <ul className="space-y-3 font-medium font-['Source_Sans_Pro'] text-base">
+                <ul className="space-y-3 md:space-y-1 font-medium font-['Source_Sans_Pro'] text-base">
                   <li>
                     <a href="#" className="">
                       FAQ
@@ -54,11 +54,11 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="border-t border-white pt-6 flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
-            <p className="text-center md:text-left font-['Source_Sans_Pro']">
+          <div className="border-t border-white pt-4 flex flex-col md:flex-row items-center justify-between">
+            <p className="mb-4 md:mb-0 text-center md:text-left font-['Source_Sans_Pro']">
               Copyright © 2025 Ayvens Carmarket. All Rights Reserved.
             </p>
-            <ul className="flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0 font-medium font-['Source_Sans_Pro'] text-sm text-center">
+            <ul className="flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0 font-medium font-['Source_Sans_Pro'] text-sm text-center md:text-left">
               <li>
                 <a href="#" className="">
                   Privacy Policy
