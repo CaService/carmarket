f7febf44451a002fd6dd9887982ebdf6bc52e748
CaService <ca.service2025@proton.me>
2025-04-04 16:26:39 +0200

    working on CarCard responsiveness

--- src/Components/AuctionPageComponents/CarCard.jsx
+++ src/Components/AuctionPageComponents/CarCard.jsx
@@ -40,11 +40,11 @@ const CarCard = () => {
       <div className="w-full mx-auto pr-6 bg-white rounded-lg shadow-md overflow-hidden mt-8">
         <div className="flex">
           {/* Immagine Auto */}
-          <div className="relative w-full md:w-96">
+          <div className="relative w-96">
             <img
               src="/images/Ayvens.svg"
               alt="Ayvens"
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover max-h-[200px] md:max-h-none"
             />
             <div className="absolute bottom-2 left-2 bg-white rounded-full w-8 h-8 flex items-center justify-center">
               <span className="text-sm font-medium">24</span>
@@ -54,7 +54,7 @@ const CarCard = () => {
           {/* Contenuto */}
           <div className="flex p-6">
             <div className="flex-col">
-              <div className="flex items-center gap-2 mb-4">
+              <div className="flex items-center gap-2 mb-4 flex-wrap md:flex-nowrap">
                 <div className="w-10 overflow-hidden rounded-full">
                   <Flag
                     code="IT"
@@ -66,7 +66,7 @@ const CarCard = () => {
                   />
                 </div>
                 <span className="font-bold text-xl font-chillax">1.</span>
-                <h2 className="text-teal-700 text-xl font-bold font-chillax">
+                <h2 className="text-teal-700 text-xl font-bold font-chillax w-full md:w-auto mt-2 md:mt-0">
                   AUDI AUDI A3 30 TDI S tronic Business S.Back Hatchback 5-door
                   (Euro 6D)
                 </h2>
@@ -74,9 +74,11 @@ const CarCard = () => {
 
               <div className="flex items-center gap-2 text-blue-600 mb-4">
                 <ClockIcon className="w-5 h-5" />
-                <span>Termina tra 2 giorno(i) 10 ora(e) 44 minuto(i)</span>
+                <span className="text-sm md:text-base">
+                  Termina tra 2 giorno(i) 10 ora(e) 44 minuto(i)
+                </span>
               </div>
-              <div className="flex gap-4 text-gray-700 text-sm font-['Source_Sans_Pro']">
+              <div className="flex flex-col md:flex-row gap-2 text-gray-700 text-sm font-['Source_Sans_Pro']">
                 <span>103.940 Chilometri | 10/11/2021</span>
                 <span>Diesel | Automatico</span>
               </div>
@@ -88,14 +90,14 @@ const CarCard = () => {
                   <p className="text-xl font-bold text-gray-800 mb-4">
                     PREZZO €
                   </p>
-                  <button className="inline-block bg-[#072534] w-90 text-center px-8 py-3 mt-16 text-white cursor-pointer rounded-full font-semibold transition duration-300 hover:bg-white hover:text-[#072534] hover:border hover:border-[#072534] font-chillax">
+                  <button className="inline-block bg-[#072534] w-full md:w-90 text-center px-8 py-3 mt-16 text-white cursor-pointer rounded-full font-semibold transition duration-300 hover:bg-white hover:text-[#072534] hover:border hover:border-[#072534] font-chillax">
                     ACQUISTA
                   </button>
                 </div>
               ) : (
                 <Link
                   to="/signup"
-                  className="inline-block bg-[#73d2d2] w-90 text-center px-8 py-3 mt-20 cursor-pointer rounded-full font-semibold transition duration-300 hover:bg-white hover:border hover:border-[#072534] font-chillax"
+                  className="inline-block bg-[#73d2d2] w-full md:w-90 text-center px-8 py-3 mt-20 cursor-pointer rounded-full font-semibold transition duration-300 hover:bg-white hover:border hover:border-[#072534] font-chillax"
                 >
                   REGISTRATI
                 </Link>
