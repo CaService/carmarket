5d8ce8adb8b71ed192f9bd71d9b72c798ad9f98a
CaService <ca.service2025@proton.me>
2025-04-18 18:23:48 +0200

    trying to roll back

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,62 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/repositories/carmarket/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    assetsDir: "",
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name]-[hash].js",
-        chunkFileNames: "[name]-[hash].js",
-        assetFileNames: (assetInfo) => {
-          const ext = assetInfo.name.split(".").pop();
-          if (ext === "css") {
-            return `style-[hash].${ext}`;
-          }
-          return "[name]-[hash].${ext}";
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    fs: {
+      allow: ["src", "public", "node_modules", "pdf", path.resolve(__dirname)],
+      strict: false,
+    },
+    proxy: {
+      "/api": {
+        target: "https://055a-82-84-9-104.ngrok-free.app/carmarket",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/server/api"),
+        headers: {
+          "ngrok-skip-browser-warning": "true",
+        },
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
         },
       },
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith(".pdf")) {
+            return "static/pdf/[name][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+      },
+    },
     cssCodeSplit: false,
-    minify: process.env.NODE_ENV === "production",
-    sourcemap: process.env.NODE_ENV !== "production",
+    assetsInlineLimit: 0,
   },
   publicDir: "public",
   assetsInclude: ["**/*.pdf"],
