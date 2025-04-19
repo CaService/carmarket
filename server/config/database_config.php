96ae2ce7bd743bdd83e504c63a6ad31e1bb04c3e
CaService <ca.service2025@proton.me>
2025-04-19 18:52:06 +0200

    trying to bring api calls to env prod

--- server/config/database_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Configurazione database di produzione
-define('DB_HOST', 'localhost:3306');  // Porta corretta dal phpMyAdmin
-define('DB_USER', 'cpses_cae60zi8iq');  // Username corretto dal phpMyAdmin
-define('DB_PASS', '7_1GCm4,]Mc&');  // Mantieni la password che hai
-define('DB_NAME', 'carmarke_carmarket_db'); 
