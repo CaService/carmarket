10783bc940680120aeb635c1792d236df8e8c7fb
CaService <ca.service2025@proton.me>
2025-04-27 00:59:17 +0200

    CardAuction changes 2

--- src/Components/CardAuction.jsx
+++ src/Components/CardAuction.jsx
@@ -5,18 +5,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCarSide } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { API_BASE_URL } from "../config/api";
+import { useSelector } from "react-redux";
 
 const CardAuction = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isAuthenticated } = useSelector((state) => state.auth);
   const [vehicles, setVehicles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  // Verifica lo stato di autenticazione
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token);
-  }, []);
 
   // Fetch dei veicoli
   useEffect(() => {
@@ -90,14 +85,14 @@ const CardAuction = () => {
                     />
                   </div>
                 </div>
-                {!isLoggedIn && (
+                {!isAuthenticated && (
                   <p className="text-center font-semibold pt-20 font-[chillax] text-lg text-[#072534]">
                     Registrati per acquistare un veicolo.
                   </p>
                 )}
 
                 <div className="mt-4 flex justify-between gap-4 pt-16">
-                  {!isLoggedIn && (
+                  {!isAuthenticated && (
                     <Link
                       to="/signup"
                       className="w-1/2 bg-[#73d2d2] text-center border border-[#73d2d2] px-3 py-3 rounded-full font-semibold transition duration-300 hover:bg-white hover:border hover:border-[#072534] flex items-center justify-center font-chillax"
@@ -108,7 +103,7 @@ const CardAuction = () => {
                   <Link
                     to="/auction"
                     className={`${
-                      isLoggedIn ? "w-full" : "w-1/2"
+                      isAuthenticated ? "w-full" : "w-1/2"
                     } bg-[#072534] border border-[#072534] text-white px-3 py-3 rounded-full font-semibold transition duration-300 hover:bg-white hover:border hover:border-[#072534] hover:text-[#072534] text-center flex items-center justify-center font-chillax`}
                   >
                     MOSTRA DI PIÙ
